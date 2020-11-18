eeb45abe27d3bf9c1b011201809eb3d82f7471a6
Pamud Udara <pamud2014@gmail.com>
2020-11-18 22:01:35 +0530

    forth commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+ @Component({
+
+    selector: 'app-server',
+    templateUrl:'./app.sever.component.html'
+
+ }
+  )
+  
+  export class SeverComponent{
+
+
+  }
