6ff64930c06869dfca7aad7afafc58dd2f7b18f6
Pamud Udara <pamud2014@gmail.com>
2020-11-22 14:48:14 +0530

    7 day 2 Modification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ // styleUrls: ['./app.component.css']
+ styles :[`
+ 
+ p{
+  color:blue;
+
+ }
+ `]
+
+
 })
 export class AppComponent {
  
