1ea9c90c1d04ed62e848a4bc9fa3d353bc1a6376
Pamud Udara <pamud2014@gmail.com>
2020-11-25 23:35:19 +0530

    9 day  Modification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    SuccessAlertComponent,
    
   ],
   imports: [
